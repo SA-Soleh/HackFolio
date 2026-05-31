33bf0b3117405ca775557d225efc271807ee0507
soleh <Lole@localhost>
2026-05-31 05:36:22 +0500

    Rebrand: changed name to Lole Jon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 import plugin from 'tailwindcss/plugin';
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
 	darkMode: ["class"],
@@ -121,7 +122,7 @@ export default {
 		}
 	},
 	plugins: [
-		require("tailwindcss-animate"),
+		tailwindcssAnimate,
 		plugin(function({ addVariant }) {
 			addVariant('light', '.light-mode &');
 		})
